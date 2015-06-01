0e31b4ff37b1db288f52b9401281a03dcee584e2
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-01 02:22:33 -0700

    Tomcat, Jersey, Maven, Ant, web.xml

---
+++ src/main/com/longmendelivery/app/HelloWorldService.java
@@ -0,0 +1,15 @@
+package org.chiaboy.webapp;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/echo")
+public class HelloWorldService {
+    @GET
+    @Path("/{param}")
+    public Response getMessage(@PathParam("param") String message) {
+        String output = "Echo Service Recieved " + message;
+        return Response.status(200).entity(output).build();
+    }
+}
