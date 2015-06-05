df9db9eeb55c90fc4df9d1a007ed3c76da5d6333
Desmond Zhou <contact@desmondzhou.com>
2015-06-05 00:46:48 -0700

    Create AppUser, Login and Order resource signature, stub out application security and throttling.

---
+++ src/main/java/com/longmendelivery/app/OrderResource.java
@@ -0,0 +1,61 @@
+package com.longmendelivery.app;
+import com.longmendelivery.lib.security.SecurityPower;
+import com.longmendelivery.lib.security.ThrottleSecurity;
+import com.longmendelivery.lib.security.TokenSecurity;
+import com.longmendelivery.persistence.model.Shipment;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("/order")
+public class OrderResource {
+    @GET
+    public Response listOrdersForUser(@QueryParam("userId") String userId, @QueryParam("token") String token) {
+        TokenSecurity.getInstance().authorize(token, SecurityPower.PRIVATE_READ, userId);
+
+        //list all active and recent order for users
+        return Response.status(200).build();
+    }
+
+    @POST
+    public Response createOrder(String order, @QueryParam("token") String token){
+        String userId = fetchOrder(order).getClient().getId();
+        TokenSecurity.getInstance().authorize(token, SecurityPower.PRIVATE_WRITE, userId);
+
+        return Response.status(200).build();
+    }
+
+    private Shipment fetchOrder(String orderId) {
+        //XXX persistence layer
+        return null;
+    }
+
+
+    @GET
+    @Path("/{orderId}")
+    public Response getOrderDetails(@PathParam("orderId") String orderId, String status, @QueryParam("token") String token){
+        TokenSecurity.getInstance().authorize(token, SecurityPower.PUBLIC_READ);
+
+        return Response.status(200).build();
+
+    }
+
+    @POST
+    @Path("/{orderId}/status")
+    public Response updateOrderStatus(@PathParam("orderId") String orderId, String status, @QueryParam("token") String token){
+        TokenSecurity.getInstance().authorize(token, SecurityPower.BACKEND_WRITE);
+
+        return Response.status(200).build();
+
+    }
+
+    @POST
+    @Path("/{orderId}/tracking")
+    public Response addTrackingNumber(@PathParam("orderId") String orderId, String trackingNumber, String trackingDocument, @QueryParam("token") String token){
+        TokenSecurity.getInstance().authorize(token, SecurityPower.BACKEND_WRITE);
+
+        return Response.status(200).build();
+
+    }
+
+}
---
+++ src/main/java/com/longmendelivery/lib/security/SecurityGroup.java
@@ -0,0 +1,8 @@
+package com.longmendelivery.lib.security;
+
+/**
+ * Created by  rabiddesireon 04/06/15.
+ */
+public enum SecurityGroup {
+    ADMIN_USER, BACKEND_USER, BACKEND_DAEMON, APP_USER, UNVERIFIED_APP_USER, GUEST;
+}
