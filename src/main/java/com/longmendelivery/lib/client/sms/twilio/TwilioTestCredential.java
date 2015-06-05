aba599509ad5236e18ac8136ce3ae3cc29fad4ed
Desmond Zhou <contact@desmondzhou.com>
2015-06-04 19:52:11 -0700

    Establish service client package, integrate Twilio SMS provider, setup debug endpoint.

---
+++ src/main/java/com/longmendelivery/lib/client/sms/twilio/TwilioTestCredential.java
@@ -0,0 +1,10 @@
+package com.longmendelivery.lib.client.sms.twilio;
+
+/**
+ * Created by  rabiddesireon 04/06/15.
+ */
+public class TwilioTestCredential {
+    public static final String ACCOUNT_SID = "AC43a5736cefd75c2c65a4268b4800f0af";
+    public static final String AUTH_TOKEN = "{{ a2e83039b60e9a23bcc5aab02fe24fb5 }}";
+    public static final String FROM = "+17786553958";
+}
