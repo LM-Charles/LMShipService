118cdfa9a6c2f8c6a35fcc8a488e01d200bb66f4
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-04 19:52:11 -0700

    Establish service client package, integrate Twilio SMS provider, setup debug endpoint.

---
+++ src/main/java/com/longmendelivery/app/DebugResource.java
@@ -0,0 +1,20 @@
+package com.longmendelivery.app;
+import com.longmendelivery.lib.client.exceptions.DependentServiceException;
+import com.longmendelivery.lib.client.sms.twilio.TwilioSMSClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/debug")
+public class DebugResource {
+    @PUT
+    @Path("testSMS/{to}/{body}")
+    public Response getMessage(@PathParam("to") String to, @PathParam("body") String body) throws DependentServiceException {
+        String output = "Testing SMS to : " + to + " with body: " + body;
+        new TwilioSMSClient().sendSMS(to, body);
+        return Response.status(200).entity(output).build();
+    }
+}
---
+++ src/main/java/com/longmendelivery/lib/client/sms/SMSClient.java
@@ -0,0 +1,10 @@
+package com.longmendelivery.lib.client.sms;
+
+import com.longmendelivery.lib.client.exceptions.DependentServiceException;
+
+/**
+ * Created by  rabiddesireon 04/06/15.
+ */
+public interface SMSClient {
+    String sendSMS(String phoneNumber, String content) throws DependentServiceException;
+}
