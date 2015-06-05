b513d66fb7128e5e09aed194b88f6ed82a3c306e
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-04 22:25:39 -0700

    Establish first generation of essential DAOs

--- src/main/java/com/longmendelivery/lib/client/exceptions/DependentServiceException.java
+++ src/main/java/com/longmendelivery/lib/client/exceptions/DependentServiceException.java
@@ -5,7 +5,8 @@ import com.twilio.sdk.TwilioRestException;
 /**
  * Created by desmond on 04/06/15.
  */
-public class DependentServiceException extends Throwable {
+public class DependentServiceException extends Exception {
     public DependentServiceException(TwilioRestException e) {
+        super(e);
     }
 }
---
+++ src/main/java/com/longmendelivery/persistence/model/AppUserActionHistory.java
@@ -0,0 +1,12 @@
+package com.longmendelivery.persistence.model;
+
+/**
+ * Created by desmond on 04/06/15.
+ */
+public class AppUserActionHistory {
+    String id;
+    String appUserId;
+
+    String action;
+    String actionDescription;
+}
