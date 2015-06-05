8163c0f5fa5dff1bc469d3966cd43d9f9aa684af
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-04 21:43:22 -0700

    Setup Hibernate persistence, spin up devo DB and connection parameters.

---
+++ src/main/java/persistence/HibernateDiagnostic.java
@@ -0,0 +1,61 @@
+package persistence;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Query;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.util.Map;
+
+/**
+ * Created by desmond on 04/06/15.
+ */
+public class HibernateDiagnostic {
+    private final SessionFactory ourSessionFactory;
+    private final ServiceRegistry serviceRegistry;
+
+    public HibernateDiagnostic(){
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public String diagnostic() throws Exception {
+        StringBuilder stringBuilder = new StringBuilder();
+        final Session session = getSession();
+        try {
+            stringBuilder.append("querying all the managed entities...");
+            stringBuilder.append("\n");
+            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
+            for (Object key : metadataMap.keySet()) {
+                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
+                final String entityName = classMetadata.getEntityName();
+                final Query query = session.createQuery("from " + entityName);
+                stringBuilder.append("executing: " + query.getQueryString());
+                stringBuilder.append("\n");
+                for (Object o : query.list()) {
+                    stringBuilder.append("  " + o);
+                    stringBuilder.append("\n");
+                }
+            }
+
+            return stringBuilder.toString();
+        } finally {
+            session.close();
+        }
+    }
+}
---
+++ src/main/java/persistence/model/AppUser.java
@@ -0,0 +1,7 @@
+package persistence.model;
+
+/**
+ * Created by desmond on 04/06/15.
+ */
+public class AppUser {
+}
