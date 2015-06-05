75fb5c1d4f41381ca171a13ccfb536fcd53ead5e
Desmond Zhou <contact@desmondzhou.com>
2015-06-04 22:25:39 -0700

    Establish first generation of essential DAOs

--- src/main/java/com/longmendelivery/persistence/model/AppUser.java
+++ src/main/java/com/longmendelivery/persistence/model/AppUser.java
@@ -4,4 +4,22 @@ package com.longmendelivery.persistence.model;
  * Created by desmond on 04/06/15.
  */
 public class AppUser {
+    String id;
+
+    String phone;
+    String email;
+    String password_md5;
+
+    String userType;
+    String userStatus;
+
+    String apiToken;
+    String verificationString;
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String province;
+    String country;
 }
---
+++ src/main/java/com/longmendelivery/persistence/model/ShipmentService.java
@@ -0,0 +1,12 @@
+package com.longmendelivery.persistence.model;
+
+/**
+ * Created by desmond on 04/06/15.
+ */
+public class ShipmentService {
+    String courierName;
+    String serviceName;
+    String serviceCode;
+    String apiProvider;
+    String lmCategoryCode;
+}
