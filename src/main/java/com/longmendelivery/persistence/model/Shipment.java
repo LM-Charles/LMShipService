495929ba1dc8432d9e0f72d0f25b655b1e2251c5
Desmond Zhou <contact@desmondzhou.com>
2015-06-05 00:46:48 -0700

    Create AppUser, Login and Order resource signature, stub out application security and throttling.

--- src/main/java/com/longmendelivery/persistence/model/Shipment.java
+++ src/main/java/com/longmendelivery/persistence/model/Shipment.java
@@ -26,6 +26,183 @@ public class Shipment {
     String fromCountry;
 
     String toAddress;
+
+    public AppUser getClient() {
+        return client;
+    }
+
+    public void setClient(AppUser client) {
+        this.client = client;
+    }
+
+    public DateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(DateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public BigDecimal getEstimateCost() {
+        return estimateCost;
+    }
+
+    public void setEstimateCost(BigDecimal estimateCost) {
+        this.estimateCost = estimateCost;
+    }
+
+    public BigDecimal getFinalCost() {
+        return finalCost;
+    }
+
+    public void setFinalCost(BigDecimal finalCost) {
+        this.finalCost = finalCost;
+    }
+
+    public String getFromAddress() {
+        return fromAddress;
+    }
+
+    public void setFromAddress(String fromAddress) {
+        this.fromAddress = fromAddress;
+    }
+
+    public String getFromCity() {
+        return fromCity;
+    }
+
+    public void setFromCity(String fromCity) {
+        this.fromCity = fromCity;
+    }
+
+    public String getFromProvince() {
+        return fromProvince;
+    }
+
+    public void setFromProvince(String fromProvince) {
+        this.fromProvince = fromProvince;
+    }
+
+    public String getFromCode() {
+        return fromCode;
+    }
+
+    public void setFromCode(String fromCode) {
+        this.fromCode = fromCode;
+    }
+
+    public String getFromCountry() {
+        return fromCountry;
+    }
+
+    public void setFromCountry(String fromCountry) {
+        this.fromCountry = fromCountry;
+    }
+
+    public String getToAddress() {
+        return toAddress;
+    }
+
+    public void setToAddress(String toAddress) {
+        this.toAddress = toAddress;
+    }
+
+    public String getToCity() {
+        return toCity;
+    }
+
+    public void setToCity(String toCity) {
+        this.toCity = toCity;
+    }
+
+    public String getToProvince() {
+        return toProvince;
+    }
+
+    public void setToProvince(String toProvince) {
+        this.toProvince = toProvince;
+    }
+
+    public String getToCode() {
+        return toCode;
+    }
+
+    public void setToCode(String toCode) {
+        this.toCode = toCode;
+    }
+
+    public String getToCountry() {
+        return toCountry;
+    }
+
+    public void setToCountry(String toCountry) {
+        this.toCountry = toCountry;
+    }
+
+    public ShipmentService getShipmentService() {
+        return shipmentService;
+    }
+
+    public void setShipmentService(ShipmentService shipmentService) {
+        this.shipmentService = shipmentService;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getTrackingDocumentType() {
+        return trackingDocumentType;
+    }
+
+    public void setTrackingDocumentType(String trackingDocumentType) {
+        this.trackingDocumentType = trackingDocumentType;
+    }
+
+    public String getHandler() {
+        return handler;
+    }
+
+    public void setHandler(String handler) {
+        this.handler = handler;
+    }
+
     String toCity;
     String toProvince;
     String toCode;
