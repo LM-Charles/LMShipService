1d93c640a8a9c2dff807668826803ba55a41f89d
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-05 00:46:48 -0700

    Create AppUser, Login and Order resource signature, stub out application security and throttling.

---
+++ src/main/java/com/longmendelivery/lib/security/SecurityPower.java
@@ -0,0 +1,8 @@
+package com.longmendelivery.lib.security;
+
+/**
+ * Created by  rabiddesireon 04/06/15.
+ */
+public enum SecurityPower {
+    ADMIN, BACKEND_WRITE, BACKEND_READ, PRIVATE_WRITE, PRIVATE_READ, PUBLIC_READ;
+}
--- src/main/java/com/longmendelivery/persistence/model/AppUser.java
+++ src/main/java/com/longmendelivery/persistence/model/AppUser.java
@@ -10,7 +10,7 @@ public class AppUser {
     String email;
     String password_md5;
 
-    String userType;
+    String userGroup;
     String userStatus;
 
     String apiToken;
@@ -22,4 +22,116 @@ public class AppUser {
     String city;
     String province;
     String country;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword_md5() {
+        return password_md5;
+    }
+
+    public void setPassword_md5(String password_md5) {
+        this.password_md5 = password_md5;
+    }
+
+    public String getUserGroup() {
+        return userGroup;
+    }
+
+    public void setUserGroup(String userGroup) {
+        this.userGroup = userGroup;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getApiToken() {
+        return apiToken;
+    }
+
+    public void setApiToken(String apiToken) {
+        this.apiToken = apiToken;
+    }
+
+    public String getVerificationString() {
+        return verificationString;
+    }
+
+    public void setVerificationString(String verificationString) {
+        this.verificationString = verificationString;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
