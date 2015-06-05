592f7d0441486037b0edab6e2c5c7c7bb99a2cea
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-04 20:48:21 -0700

    Add shipping model and package structure

---
+++ src/main/java/com/longmendelivery/lib/client/shipment/ShipmentClient.java
@@ -0,0 +1,14 @@
+package com.longmendelivery.lib.client.shipment;
+
+import com.longmendelivery.lib.client.shipment.model.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ * Created by desmond on 04/06/15.
+ */
+public interface ShipmentClient {
+    Map<ShippingService, BigDecimal> getRates(ShippingAddress shippingAddress, ShippingDimension dimension);
+    TrackingRecord getTracking(String trackingNumber, TrackingDocumentType trackingDocumentType);
+}
