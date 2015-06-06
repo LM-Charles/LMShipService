252c172a4b1c37bb3ec8ff9fb3b7eed598a4a667
Desmond Zhou <contact@desmondzhou.com>
2015-06-05 22:14:42 -0700

    Add DAO Hibernate Annotation, create Hibernate session manager

--- src/main/java/com/longmendelivery/persistence/model/AppUserActionHistory.java
+++ src/main/java/com/longmendelivery/persistence/model/AppUserActionHistory.java
@@ -1,12 +1,92 @@
 package com.longmendelivery.persistence.model;
 
+import javax.persistence.*;
+
 /**
  * Created by desmond on 04/06/15.
  */
-class AppUserActionHistory {
-    String id;
-    String appUserId;
 
+@Entity
+@Table(name = "APP_USER_ACTION_HISTORY")
+class AppUserActionHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    String id;
+
+    @ManyToOne
+    @JoinColumn(name = "APP_USER_ID", nullable = false)
+    AppUser appUserId;
+
+    @Column(name = "ACTION", nullable = false)
     String action;
+
+    @Column(name = "ACTION_DESCRIPTION", nullable = false)
     String actionDescription;
+
+    public AppUserActionHistory() {
+    }
+
+    public AppUserActionHistory(String id, AppUser appUserId, String action, String actionDescription) {
+
+        this.id = id;
+        this.appUserId = appUserId;
+        this.action = action;
+        this.actionDescription = actionDescription;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AppUserActionHistory that = (AppUserActionHistory) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (appUserId != null ? !appUserId.equals(that.appUserId) : that.appUserId != null) return false;
+        if (action != null ? !action.equals(that.action) : that.action != null) return false;
+        return !(actionDescription != null ? !actionDescription.equals(that.actionDescription) : that.actionDescription != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (appUserId != null ? appUserId.hashCode() : 0);
+        result = 31 * result + (action != null ? action.hashCode() : 0);
+        result = 31 * result + (actionDescription != null ? actionDescription.hashCode() : 0);
+        return result;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AppUser getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(AppUser appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getActionDescription() {
+        return actionDescription;
+    }
+
+    public void setActionDescription(String actionDescription) {
+        this.actionDescription = actionDescription;
+    }
 }
