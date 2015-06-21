aa8bf8217aac1ea0f80b5cf9565cee26d02b1b23
Desmond Zhou <contact@desmondzhou.com>
2015-06-21 02:52:23 -0700

    Add stub for order status calculation

---
+++ src/main/java/com/longmendelivery/lib/business/OrderStatusCalculator.java
@@ -0,0 +1,13 @@
+package com.longmendelivery.lib.business;
+
+import com.longmendelivery.app.model.OrderStatusModel;
+import com.longmendelivery.persistence.entity.OrderEntity;
+
+/**
+ * Created by  rabiddesireon 21/06/15.
+ */
+public class OrderStatusCalculator {
+    public static OrderStatusModel calculate(OrderEntity order) {
+        return null;
+    }
+}
