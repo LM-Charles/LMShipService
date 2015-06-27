27174a682af3fa36bf1ed7461047090cf9a1ef0a
rabiddesire <rabiddesire@users.noreply.github.com>
2015-06-27 11:42:46 -0700

    Fix shallow health check

--- src/main/java/com/longmendelivery/service/EchoResource.java
+++ src/main/java/com/longmendelivery/service/EchoResource.java
@@ -5,9 +5,11 @@ import com.longmendelivery.service.util.ResourceResponseUtil;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/ping")
+@Produces("application/json")
 public class EchoResource {
     @GET
     public Response getMessage() {
