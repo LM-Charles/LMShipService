2e5f2d869d78f13a0e7b7d013dc617f1f553742b
Desmond Zhou <contact@desmondzhou.com>
2015-06-27 11:02:46 -0700

    Replace deprecated hiberate init code

--- src/main/java/com/longmendelivery/persistence/util/HibernateUtil.java
+++ src/main/java/com/longmendelivery/persistence/util/HibernateUtil.java
@@ -1,16 +1,22 @@
 package com.longmendelivery.persistence.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
-@SuppressWarnings("deprecation")
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
 
     static {
         try {
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                    configuration.getProperties()).build();
+
+            sessionFactory = new Configuration().configure().buildSessionFactory(serviceRegistry);
 
         } catch (Throwable ex) {
             System.err.println("Session Factory could not be created." + ex);
