60a6c7d372087028a7dd656f825701cef94d7735
Desmond Zhou <contact@desmondzhou.com>
2015-07-05 11:38:15 -0700

    Create the model for https://github.com/LM-Charles/LMShipService/issues/4 Introduce Lombok Install IDE Lombok Plugin and Enable Annotation Pre-processor Lombok Annotate all POJO

--- src/main/java/com/longmendelivery/service/model/AppUserModel.java
+++ src/main/java/com/longmendelivery/service/model/AppUserModel.java
@@ -5,263 +5,53 @@ import com.longmendelivery.lib.conversion.Model;
 import com.longmendelivery.persistence.entity.AppUserGroupEntity;
 import com.longmendelivery.persistence.entity.AppUserStatusEntity;
 import com.longmendelivery.persistence.entity.OrderEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.Set;
 
 /**
  * Created by  rabiddesireon 04/06/15.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUserModel implements Model {
 
     @JsonProperty
+    @NonNull
     private Integer id;
     @JsonProperty
+    @NonNull
     private String phone;
     @JsonProperty
+    @NonNull
     private String email;
     @JsonProperty
+    @NonNull
     private String password_md5;
     @JsonProperty
+    @NonNull
     private AppUserGroupEntity userGroup;
     @JsonProperty
+    @NonNull
     private AppUserStatusEntity userStatus;
     @JsonProperty
+    @NonNull
     private String apiToken;
     @JsonProperty
-    private String verificationString;
+    @NonNull
+    private VerificationCodeModel verificationCode;
+
     @JsonProperty
     private String firstName;
     @JsonProperty
     private String lastName;
     @JsonProperty
-    private String address;
-    @JsonProperty
-    private String city;
-    @JsonProperty
-    private String province;
-    @JsonProperty
-    private String country;
+    private AddressModel address;
 
+    @JsonProperty
     private Set<OrderEntity> orders;
-
-    public AppUserModel(String phone, String email, String password_md5, AppUserGroupEntity userGroup, AppUserStatusEntity userStatus) {
-        this.phone = phone;
-        this.email = email;
-        this.password_md5 = password_md5;
-        this.userGroup = userGroup;
-        this.userStatus = userStatus;
-    }
-
-
-    public AppUserModel(Integer id, String phone, String email, String password_md5, AppUserGroupEntity userGroup, AppUserStatusEntity userStatus, String apiToken, String verificationString, String firstName, String lastName, String address, String city, String province, String country, Set<OrderEntity> orders) {
-        this.id = id;
-        this.phone = phone;
-        this.email = email;
-        this.password_md5 = password_md5;
-        this.userGroup = userGroup;
-        this.userStatus = userStatus;
-        this.apiToken = apiToken;
-        this.verificationString = verificationString;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.province = province;
-        this.country = country;
-        this.orders = orders;
-    }
-
-    public AppUserModel() {
-
-    }
-
-    @Override
-    public String
-    toString() {
-        return "AppUserEntity{" +
-                "id=" + id +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", password_md5='" + password_md5 + '\'' +
-                ", userGroup=" + userGroup +
-                ", userStatus=" + userStatus +
-                ", apiToken='" + apiToken + '\'' +
-                ", verificationString='" + verificationString + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", province='" + province + '\'' +
-                ", country='" + country + '\'' +
-                ", orders=" + orders +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AppUserModel appUser = (AppUserModel) o;
-
-        if (id != null ? !id.equals(appUser.id) : appUser.id != null) return false;
-        if (phone != null ? !phone.equals(appUser.phone) : appUser.phone != null) return false;
-        if (email != null ? !email.equals(appUser.email) : appUser.email != null) return false;
-        if (password_md5 != null ? !password_md5.equals(appUser.password_md5) : appUser.password_md5 != null)
-            return false;
-        if (userGroup != appUser.userGroup) return false;
-        if (userStatus != appUser.userStatus) return false;
-        if (apiToken != null ? !apiToken.equals(appUser.apiToken) : appUser.apiToken != null) return false;
-        if (verificationString != null ? !verificationString.equals(appUser.verificationString) : appUser.verificationString != null)
-            return false;
-        if (firstName != null ? !firstName.equals(appUser.firstName) : appUser.firstName != null) return false;
-        if (lastName != null ? !lastName.equals(appUser.lastName) : appUser.lastName != null) return false;
-        if (address != null ? !address.equals(appUser.address) : appUser.address != null) return false;
-        if (city != null ? !city.equals(appUser.city) : appUser.city != null) return false;
-        if (province != null ? !province.equals(appUser.province) : appUser.province != null) return false;
-        if (country != null ? !country.equals(appUser.country) : appUser.country != null) return false;
-        return !(orders != null ? !orders.equals(appUser.orders) : appUser.orders != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (phone != null ? phone.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + (password_md5 != null ? password_md5.hashCode() : 0);
-        result = 31 * result + (userGroup != null ? userGroup.hashCode() : 0);
-        result = 31 * result + (userStatus != null ? userStatus.hashCode() : 0);
-        result = 31 * result + (apiToken != null ? apiToken.hashCode() : 0);
-        result = 31 * result + (verificationString != null ? verificationString.hashCode() : 0);
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + (address != null ? address.hashCode() : 0);
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        result = 31 * result + (province != null ? province.hashCode() : 0);
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        result = 31 * result + (orders != null ? orders.hashCode() : 0);
-        return result;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword_md5() {
-        return password_md5;
-    }
-
-    public void setPassword_md5(String password_md5) {
-        this.password_md5 = password_md5;
-    }
-
-    public AppUserGroupEntity getUserGroup() {
-        return userGroup;
-    }
-
-    public void setUserGroup(AppUserGroupEntity userGroup) {
-        this.userGroup = userGroup;
-    }
-
-    public AppUserStatusEntity getUserStatus() {
-        return userStatus;
-    }
-
-    public void setUserStatus(AppUserStatusEntity userStatus) {
-        this.userStatus = userStatus;
-    }
-
-    public String getApiToken() {
-        return apiToken;
-    }
-
-    public void setApiToken(String apiToken) {
-        this.apiToken = apiToken;
-    }
-
-    public String getVerificationString() {
-        return verificationString;
-    }
-
-    public void setVerificationString(String verificationString) {
-        this.verificationString = verificationString;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Set<OrderEntity> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(Set<OrderEntity> orders) {
-        this.orders = orders;
-    }
 }
