64031dd5c7008e8e062dcf07c323fff2a223b89d
Desmond Zhou <contact@desmondzhou.com>
2015-07-05 11:50:18 -0700

    Create rate calculator resource and POJO

---
+++ src/main/java/com/longmendelivery/service/behavior/RateCalculator.java
@@ -0,0 +1,11 @@
+package com.longmendelivery.service.behavior;
+
+import com.longmendelivery.service.model.request.RateRequestModel;
+import com.longmendelivery.service.model.response.RateResponseModel;
+
+/**
+ * Created by  rabiddesireon 05/07/15.
+ */
+public interface RateCalculator {
+    RateResponseModel calculate(RateRequestModel rateRequestModel);
+}
