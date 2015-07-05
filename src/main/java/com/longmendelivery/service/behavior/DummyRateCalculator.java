12b26b8a586e8bd2d5e1694760947d83005bc421
Desmond Zhou <contact@desmondzhou.com>
2015-07-05 11:50:18 -0700

    Create rate calculator resource and POJO

---
+++ src/main/java/com/longmendelivery/service/behavior/DummyRateCalculator.java
@@ -0,0 +1,7 @@
+package com.longmendelivery.service.behavior;
+
+/**
+ * Created by desmond on 05/07/15.
+ */
+public class DummyRateCalculator implements RateCalculator {
+}
---
+++ src/main/java/com/longmendelivery/service/model/RateEntryModel.java
@@ -0,0 +1,36 @@
+package com.longmendelivery.service.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by desmond on 05/07/15.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RateEntryModel {
+    @JsonProperty
+    @NonNull
+    private String service_icon_url;
+    @JsonProperty
+    @NonNull
+    private String LMCategory;
+    @JsonProperty
+    @NonNull
+    private BigDecimal estimate;
+    @JsonProperty
+    @NonNull
+    private String courierName;
+    @JsonProperty
+    @NonNull
+    private String serviceName;
+    @JsonProperty
+    private DateTime estimatedDelivery;
+}
