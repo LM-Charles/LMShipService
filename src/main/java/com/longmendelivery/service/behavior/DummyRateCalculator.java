3f3345c237f7e7232ffb6c62111627cd738d2834
Desmond Zhou <contact@desmondzhou.com>
2015-07-05 12:00:52 -0700

    Implement dummy rate calculator

--- src/main/java/com/longmendelivery/service/behavior/DummyRateCalculator.java
+++ src/main/java/com/longmendelivery/service/behavior/DummyRateCalculator.java
@@ -1,7 +1,23 @@
 package com.longmendelivery.service.behavior;
 
+import com.longmendelivery.service.model.RateEntryModel;
+import com.longmendelivery.service.model.request.RateRequestModel;
+import com.longmendelivery.service.model.response.RateResponseModel;
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by  rabiddesireon 05/07/15.
  */
 public class DummyRateCalculator implements RateCalculator {
+    @Override
+    public RateResponseModel calculate(RateRequestModel rateRequestModel) {
+        List<RateEntryModel> rates = new ArrayList<>();
+        RateEntryModel dummyModel = new RateEntryModel("", "Dummy", BigDecimal.ONE, "DummyCourier", "DummyService", DateTime.now());
+        rates.add(dummyModel);
+        return new RateResponseModel(DateTime.now(), rates);
+    }
 }
