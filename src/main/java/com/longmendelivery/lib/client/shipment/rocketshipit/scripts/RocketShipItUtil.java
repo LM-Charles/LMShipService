458dedc19e2a01516d59c4930d365a1dccb88750
rabiddesire <rabiddesire@users.noreply.github.com>
2015-08-09 14:43:23 -0700

    Thread through the environmental based configuration for loading library

---
+++ src/main/java/com/longmendelivery/lib/client/shipment/rocketshipit/scripts/RocketShipItUtil.java
@@ -0,0 +1,17 @@
+package com.longmendelivery.lib.client.shipment.rocketshipit.scripts;
+
+import com.longmendelivery.service.initializer.EnvironmentStage;
+import com.longmendelivery.service.initializer.EnvironmentUtil;
+
+/**
+ * Created by desmond on 09/08/15.
+ */
+public class RocketShipItUtil {
+    public static String generateLineToRequireRocketShipItLibrary() {
+        if (EnvironmentUtil.getStage().equals(EnvironmentStage.DESKTOP)) {
+            return "require './src/main/php/php-rocket-shipit/autoload.php';";
+        } else {
+            return "require '/var/lib/tomcat7/webapps/ROOT/WEB-INF/classes/php-rocket-shipit/autoload.php';";
+        }
+    }
+}
