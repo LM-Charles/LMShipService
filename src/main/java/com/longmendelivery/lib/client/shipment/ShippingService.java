036d0e8e0400f6bc417b3414984ca4007ab82c65
rabiddesire <rabiddesire@users.noreply.github.com>
2015-08-14 15:31:04 -0700

    Wire up the parsing of courier and service code from UPS

--- src/main/java/com/longmendelivery/lib/client/shipment/ShippingService.java
+++ src/main/java/com/longmendelivery/lib/client/shipment/ShippingService.java
@@ -1,13 +1,30 @@
 package com.longmendelivery.lib.client.shipment;
 
+import com.longmendelivery.lib.client.shipment.rocketshipit.model.CourierType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Created by desmond on 04/06/15.
  */
+@AllArgsConstructor
+@Getter
 public enum ShippingService {
-    DUMMY;
+    UPS_STANDARD(CourierType.UPS, "UPS Standard", "11"),
+    UPS_WORLDWIDE_EXPEDITED(CourierType.UPS, "UPS Worldwide Expedited", "02"),
+    UPS_SAVER(CourierType.UPS, "UPS Saver", "13"),
+    UPS_EXPRESS(CourierType.UPS, "UPS Express", "01");
 
-    public static ShippingService getFromServiceCode(String serviceCode) {
-        //XXX
+    public static ShippingService getFromServiceCode(CourierType courier, String serviceCode) {
+        for (ShippingService service : ShippingService.values()) {
+            if (service.getCourier().equals(courier) && service.getServiceCode().equals(serviceCode)) {
+                return service;
+            }
+        }
         return null;
     }
+
+    private CourierType courier;
+    private String description;
+    private String serviceCode;
 }
