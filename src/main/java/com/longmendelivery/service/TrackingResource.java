39f7d272f697179ed0edee068692873eb09a40d1
Desmond Zhou <contact@desmondzhou.com>
2015-08-14 16:59:03 -0700

    Wire up serialization into a Json Tree

---
+++ src/main/java/com/longmendelivery/service/TrackingResource.java
@@ -0,0 +1,30 @@
+package com.longmendelivery.service;
+
+import com.longmendelivery.lib.client.exceptions.DependentServiceException;
+import com.longmendelivery.lib.client.shipment.rocketshipit.RocketShipShipmentClient;
+import com.longmendelivery.lib.client.shipment.rocketshipit.model.CourierType;
+import com.longmendelivery.lib.client.shipment.rocketshipit.model.TrackingRecord;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("/tracking")
+@Produces("application/json")
+public class TrackingResource {
+    private RocketShipShipmentClient client;
+
+    public TrackingResource() throws DependentServiceException {
+        client = new RocketShipShipmentClient();
+    }
+
+    @GET
+    public Response getRate(@QueryParam("trackingNumber") String trackingNumber, @QueryParam("courier") String courierCode, @QueryParam("token") String token) throws DependentServiceException {
+        CourierType type = CourierType.valueOf(courierCode);
+        TrackingRecord trackingResult = client.getTracking(type, trackingNumber);
+
+        return Response.status(Response.Status.OK).build();
+    }
+}
---
+++ src/main/java/com/longmendelivery/service/model/response/TrackingResponseModel.java
@@ -0,0 +1,20 @@
+package com.longmendelivery.service.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.joda.time.DateTime;
+
+/**
+ * Created by desmond on 05/07/15.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrackingResponseModel {
+    @JsonProperty
+    @NonNull
+    DateTime trackingDate;
+}
