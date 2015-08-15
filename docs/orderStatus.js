fe3f6374bdf13fc17a742545882fe0dd974377a3
rabiddesire <rabiddesire@users.noreply.github.com>
2015-08-15 10:53:28 -0700

    Based on courier information, working out the details of the response for getting order status and package tracking

---
+++ docs/orderStatus.js
@@ -0,0 +1,38 @@
+var orderStatusResponse = {
+    "status": "##represent the status of the order for LM##",
+    "serviceInfo": {
+        "serviceId": "##the service id assigned by LM to manage the service##",
+        "serviceCategory": "##the service category assigned by LM to manage the service##",
+        "courier": "## the underlying courier name: eg UPS ##",
+        "courierServiceCode": "## the code used by courier to represent the service, eg. 01 ##",
+        "courierServiceDescription": "## the descriptor used by courier to display the service 3 Day Air ##"
+    },
+    "tracking": {
+        "trackingNumber": "## string for searching this tracking ##",
+        "trackingURL": "## the URL for accessing the tracking page for this shipment ##",
+        "startDate": "## timestamp for the shipment confirmed to be in courier system ##",
+        "packages": [
+            {
+                "trackingNumber": "## tracking number for the individual package under this shipment",
+                "activityStatus": "## most recent status, courier provided human readable status, eg. DELIVERED ##",
+                "activityDate": "## timestamp of most recent status update",
+                "activityLocation": "## address string for the location of this activity ##"
+            },
+            {
+                "trackingNumber": "## tracking number for the individual package under this shipment",
+
+                "status": "## courier provided human readable status ##",
+                "statusDate": "## timestamp of this status update"
+            }
+        ]
+    }
+};
+
+
+// orderStatus represent the status of the order for LM
+// ORDER_PLACED - Customer confirmed the order, driver appointment is set to pick up
+// PROCESSING - Picked up and with LM, before sent to courier and obtaining tracking number
+// IN_TRANSIT - After entering the tracking number and handed over to courier
+// COMPLETE - Confirmed to be complete
+
+// shipmentTracking is only present if the order is IN_TRANSIT, and contains courier information
