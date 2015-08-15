1a848f7990f5133c8adaba6d798bd60781eb8bc0
Desmond Zhou <contact@desmondzhou.com>
2015-08-15 14:21:36 -0700

    Added good category, inssurance value and declared value

---
+++ src/main/java/com/longmendelivery/persistence/entity/GoodCategory.java
@@ -0,0 +1,8 @@
+package com.longmendelivery.persistence.entity;
+
+/**
+ * Created by desmond on 15/08/15.
+ */
+public enum GoodCategory {
+    REGULAR, LUXURY, COSMETICS
+}
--- src/main/java/com/longmendelivery/service/model/request/OrderCreationRequestModel.java
+++ src/main/java/com/longmendelivery/service/model/request/OrderCreationRequestModel.java
@@ -2,6 +2,7 @@ package com.longmendelivery.service.model.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.longmendelivery.lib.conversion.Model;
+import com.longmendelivery.persistence.entity.GoodCategory;
 import com.longmendelivery.service.model.AddressModel;
 import com.longmendelivery.service.model.CourierServiceType;
 import com.longmendelivery.service.model.ShipmentModel;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.joda.time.DateTime;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 /**
@@ -43,5 +45,12 @@ public class OrderCreationRequestModel implements Model {
     @JsonProperty
     @NonNull
     private Set<ShipmentModel> shipments;
+
+    @JsonProperty
+    private GoodCategory goodCategory;
+    @JsonProperty
+    private BigDecimal declareValue;
+    @JsonProperty
+    private BigDecimal insuranceValue;
 
 }
