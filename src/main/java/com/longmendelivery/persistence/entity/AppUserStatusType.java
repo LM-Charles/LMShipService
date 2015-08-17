433f82418c7677b9f8700aae01e2b335fbd43ab3
rabiddesire <rabiddesire@users.noreply.github.com>
2015-08-16 23:06:40 -0700

    Streamline Entity with Lombok

--- src/main/java/com/longmendelivery/persistence/entity/AppUserStatusType.java
+++ src/main/java/com/longmendelivery/persistence/entity/AppUserStatusType.java
@@ -3,6 +3,6 @@ package com.longmendelivery.persistence.entity;
 /**
  * Created by  rabiddesireon 17/06/15.
  */
-public enum AppUserStatusEntity {
+public enum AppUserStatusType {
     NEW, PENDING_VERIFICATION_REGISTER, ACTIVE, DISABLED
 }
