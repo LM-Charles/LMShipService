8d5946a0e9939927c875be4f1f1f105e464aafae
Desmond Zhou <contact@desmondzhou.com>
2015-08-16 23:06:40 -0700

    Streamline Entity with Lombok

--- src/main/java/com/longmendelivery/lib/conversion/EntityToIdCustomConverter.java
+++ src/main/java/com/longmendelivery/lib/conversion/EntityToIdCustomConverter.java
@@ -1,6 +1,11 @@
 package com.longmendelivery.lib.conversion;
 
+import com.longmendelivery.persistence.util.HibernateUtil;
 import org.dozer.CustomConverter;
+import org.dozer.MappingException;
+import org.hibernate.Session;
+
+import java.io.Serializable;
 
 /**
  * Created by desmond on 21/06/15.
@@ -8,6 +13,23 @@ import org.dozer.CustomConverter;
 public class EntityToIdCustomConverter implements CustomConverter {
     @Override
     public Object convert(Object dest, Object src, Class<?> destClass, Class<?> srcClass) {
-        return ((DAOEntity) src).getId();
+        if (src == null) {
+            return null;
+        }
+
+        if (src instanceof DAOEntity) {
+            return ((DAOEntity) src).getId();
+        } else if (src instanceof Integer) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            try {
+                return session.get(destClass, (Serializable) src);
+            } catch (Exception e) {
+                throw new MappingException(e);
+            } finally {
+                session.close();
+            }
+        } else {
+            throw new MappingException("EntityToIdCustomConverter " + "used incorrectly. Arguments passed in were:" + dest + " and " + src);
+        }
     }
 }
