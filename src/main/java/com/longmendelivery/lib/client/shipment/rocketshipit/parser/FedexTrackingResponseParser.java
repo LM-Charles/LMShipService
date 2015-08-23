59c4ec70d624721b87b8312c16363f4c8065586b
Desmond Zhou <contact@desmondzhou.com>
2015-08-23 16:42:33 -0700

    Complete rate for Fedex and UPS

--- src/main/java/com/longmendelivery/lib/client/shipment/rocketshipit/parser/FedexTrackingResponseParser.java
+++ src/main/java/com/longmendelivery/lib/client/shipment/rocketshipit/parser/FedexTrackingResponseParser.java
@@ -20,14 +20,15 @@ public class FedexTrackingResponseParser implements TrackingResponseParser {
     //            'Residential' => "false"
     @Override
     public ShipmentTrackingResponse parseResponse(JsonNode jsonNode) throws DependentServiceException {
-        String status = jsonNode.path("/TrackReply/Notifications/Severity").asText();
+        System.out.println(jsonNode.toString());
+        String status = jsonNode.at("/TrackReply/Notifications/Severity").asText();
         if (!status.equals("SUCCESS"))
             throw new DependentServiceException("Tracking request did not return success for Fedex");
-        DateTime pickUpDate = DateTime.parse(jsonNode.path("/TrackReply/TrackDetails/ShipTimestamp").asText());
-        DateTime trackingDate = DateTime.parse(jsonNode.path("/TrackReply/TrackDetails/Events/0/Timestamp").asText());
-        String trackingCity = jsonNode.path("/TrackReply/TrackDetails/Events/0/Address/City").asText();
-        String trackingCountry = jsonNode.path("/TrackReply/TrackDetails/Events/0/Address/CountryCode").asText();
-        String trackingStatus = jsonNode.path("/TrackReply/TrackDetails/Events/0/EventDescription").asText();
+        DateTime pickUpDate = DateTime.parse(jsonNode.at("/TrackReply/TrackDetails/ShipTimestamp").asText());
+        DateTime trackingDate = DateTime.parse(jsonNode.at("/TrackReply/TrackDetails/Events/0/Timestamp").asText());
+        String trackingCity = jsonNode.at("/TrackReply/TrackDetails/Events/0/Address/City").asText();
+        String trackingCountry = jsonNode.at("/TrackReply/TrackDetails/Events/0/Address/CountryCode").asText();
+        String trackingStatus = jsonNode.at("/TrackReply/TrackDetails/Events/0/EventDescription").asText();
         ShipmentTrackingResponse model = new ShipmentTrackingResponse(pickUpDate, trackingDate, trackingCity, trackingCountry, trackingStatus);
         return model;
     }
