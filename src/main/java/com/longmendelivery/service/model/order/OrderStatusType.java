8dd11cb83cc66349ed9131e04061da7a43466dd4
Desmond Zhou <contact@desmondzhou.com>
2015-09-16 20:54:37 -0700

    https://github.com/LM-Charles/LMShipService/issues/56

--- src/main/java/com/longmendelivery/service/model/order/OrderStatusType.java
+++ src/main/java/com/longmendelivery/service/model/order/OrderStatusType.java
@@ -4,5 +4,5 @@ package com.longmendelivery.service.model.order;
  * Created by  rabiddesireon 23/08/15.
  */
 public enum OrderStatusType {
-    ORDER_PLACED, PROCESSING, IN_TRANSIT, ERROR, COMPLETE
+    ORDER_PLACED, PROCESSING, IN_TRANSIT, ERROR, COMPLETE, CANCELLED
 }
