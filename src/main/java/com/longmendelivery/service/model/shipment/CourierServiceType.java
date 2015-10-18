5907819eda6f72a902cd5c0f6d49d60c89853c10
rabiddesire <rabiddesire@users.noreply.github.com>
2015-10-18 14:03:26 -0700

    Add more service type

--- src/main/java/com/longmendelivery/service/model/shipment/CourierServiceType.java
+++ src/main/java/com/longmendelivery/service/model/shipment/CourierServiceType.java
@@ -15,14 +15,27 @@ public enum CourierServiceType {
     UPS_SAVER(CourierType.UPS, "UPS Saver", "13", "ECONOMY_COURIER", "/public/image/ups.png"),
     UPS_EXPRESS(CourierType.UPS, "UPS Express", "01", "FASTEST_COURIER", "/public/image/ups.png"),
     UPS_EXPRESS_EARLY_AM(CourierType.UPS, "UPS Express Early AM", "14", "FASTEST_COURIER", "/public/image/ups.png"),
+    UPS_INTERNATIONAL_FIRST(CourierType.UPS, "UPS International First", "07", "FASTEST_COURIER", "/public/image/ups.png"),
+    UPS_INTERNATIONAL_PRIORITY(CourierType.UPS, "UPS International Priority", "08", "MEDIUM_COURIER", "/public/image/ups.png"),
+    UPS_INTERNATIONAL_SAVER(CourierType.UPS, "UPS World Wide Saver", "65", "ECONOMY_COURIER", "/public/image/ups.png"),
 
     FEDEX_GROUND(CourierType.FEDEX, "FedEx Ground", "FEDEX_GROUND", "ECONOMY_COURIER", "/public/image/fedex.jpg"),
 
-    CANADA_EXPEDITED(CourierType.CANADA_POST, "Canada Expedited Post", "DOM.EP", "FASTEST_COURIER", "/public/image/canadapost.jpg"),
-    CANADA_PRIORITY(CourierType.CANADA_POST, "Canada Priority Post", "DOM.PC", "MEDIUM_COURIER", "/public/image/canadapost.jpg"),
-    CANADA_REGULAR(CourierType.CANADA_POST, "Canada Regular Post", "DOM.RP", "ECONOMY_COURIER", "/public/image/canadapost.jpg"),
-    CANADA_EXPRESS(CourierType.CANADA_POST, "Canada Express Post", "DOM.XP", "FASTEST_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_EXPEDITED(CourierType.CANADA_POST, "Expedited Parcel", "DOM.EP", "FASTEST_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_PRIORITY(CourierType.CANADA_POST, "Priority", "DOM.PC", "MEDIUM_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_REGULAR(CourierType.CANADA_POST, "Regular Parcel", "DOM.RP", "ECONOMY_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_EXPRESS(CourierType.CANADA_POST, "Xpresspost", "DOM.XP", "FASTEST_COURIER", "/public/image/canadapost.jpg"),
 
+    CANADA_INTERNATIONAL_SMALL_AIR(CourierType.CANADA_POST, "Small Packet International Air", "INT.SP.AIR", "MEDIUM_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_INTERNATIONAL_SMALL_SURFACE(CourierType.CANADA_POST, "Small Packet International Surface", "INT.SP.SURF", "ECONOMY_COURIER", "/public/image/canadapost.jpg"),
+
+    CANADA_INTERNATIONAL_SURFACE(CourierType.CANADA_POST, "International Parcel Surface", "INT.IP.SURF", "ECONOMY_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_INTERNATIONAL_TRACKED_PARCEL(CourierType.CANADA_POST, "International Tracked Parcel", "INT.TP", "ECONOMY_COURIER", "/public/image/canadapost.jpg"),
+
+    CANADA_INTERNATIONAL_PAK(CourierType.CANADA_POST, "Priority Worldwide Pak", "INT.PW.PAK", "MEDIUM_COURIER", "/public/image/canadapost.jpg"),
+    CANADA_INTERNATIONAL_PARCEL(CourierType.CANADA_POST, "Priority Worldwide Parcel", "INT.PW.PAK", "MEDIUM_COURIER", "/public/image/canadapost.jpg"),
+
+    CANADA_INTERNATIONAL_EXPRESS(CourierType.CANADA_POST, "Xpresspost International", "INT.XP", "FASTEST_COURIER", "/public/image/canadapost.jpg"),
 
     LONGMEN_STANDARD(CourierType.LONGMEN, "Long Men Standard", "LONGMEN_STANDARD", "MEDIUM_COURIER", "/public/image/longmen.jpg"),
 
