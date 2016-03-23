09c3e62df713d641fbe358708d37c363608bb897
Desmond Zhou <contact@desmondzhou.com>
2016-03-23 09:34:47 -0700

    Replace Account SK API Key with AC API Key

--- src/main/java/com/longmendelivery/lib/client/sms/twilio/TwilioProdCredentials.java
+++ src/main/java/com/longmendelivery/lib/client/sms/twilio/TwilioProdCredentials.java
@@ -4,8 +4,8 @@ package com.longmendelivery.lib.client.sms.twilio;
  * Created by rabiddesire on 04/06/15.
  */
 class TwilioProdCredentials {
-    public static final String ACCOUNT_SID = "SKda8d34ae88397694c583bd94f5d3598c";
-    public static final String AUTH_TOKEN = "LIQOb4NvChPJL9HHMIDONI6TjuXgcpoK";
+    public static final String ACCOUNT_SID = "ACf8731127576a02ea1c0946872fd27447";
+    public static final String AUTH_TOKEN = "20a87f670abba7fb7ffa535e649dc77c";
     public static final String FROM = "+17786519800";
 }
 
